ca4f195f28b9a08fc54aaac2291fb8214318edc4
P1voZZZavr <yaroslav.velker@gmail.com>
2026-01-27 21:03:22 +0300

    end_auth

--- reg.js
+++ reg.js
@@ -3,13 +3,17 @@ const login = document.querySelector("#login")
 const signin = document.querySelector("#signin")
 const signup = document.querySelector("#signup")
 const loginError = document.querySelector(".login_error")
+const loginErrorText = document.querySelector(".login_error_text")
+const loginSuccessful = document.querySelector(".login_successful")
+const loginSuccessfulText = document.querySelector(".login_successful_text")
 
+menuBtn()
 
 function menuBtn() {
-    let menuBtn = document.querySelector("#menuBtn")
-    let name = localStorage.getItem(name)
-    if (name != null){
-        menuBtn.innerText = name
+    let menuBtn = document.querySelector("#signinBtn")
+    let username = localStorage.getItem("name")
+    if (username != null){
+        menuBtn.innerText = username[0].toUpperCase() + username.slice(1)
     }
 }
 
@@ -51,28 +55,54 @@ async function fetchServerGet(name){
 }
 
 
-signup.addEventListener("click", (event)=>{
+signup.addEventListener("click", async (event)=>{
     event.preventDefault()
     let data = {}
     let loginForm = new FormData(login)
-    let name = loginForm.get("name")
+    let name = loginForm.get("name").toLowerCase()
     data.pass = loginForm.get("pass")
-    let get = fetchServerGet(name)
-    if (get.pass != null){
-        fetchServerPost(data, name)
+    let get = await fetchServerGet(name)
+    if (get.pass == null){
+        await fetchServerPost(data, name)
+        loginSuccessful.style.display = "block"
+        loginSuccessfulText.innerText = "Успешная регистрация"
     }
     else {
+        loginSuccessful.style.display = "none"
+        loginErrorText.innerText = "Такой пользователь уже зарегестрирован"
         loginError.style.display = "block";
+        setTimeout(()=>{
+            location.reload()
+        },1000)
     }
 })
 
 signin.addEventListener("click", async (event)=>{
     event.preventDefault()
     let loginForm = new FormData(login)
-    let name = loginForm.get("name")
+    let name = loginForm.get("name").toLowerCase()
     let pass = loginForm.get("pass")
     let get = await fetchServerGet(name)
     if (get.pass == pass){
-        localStorage.setItem("name",name)
+        localStorage.setItem("name",name)   
+        loginSuccessful.style.display = "block"
+        loginSuccessfulText.innerText = "Успешный вход"
+        menuBtn()
+    }
+    else if(get.pass == null){
+        loginSuccessful.style.display = "none"
+        loginErrorText.innerText = "Такой пользователь еще не зарегестрирован"
+        loginError.style.display = "block";
+        setTimeout(()=>{
+            location.reload()
+        },1000)
+    }
+    else {
+        loginSuccessful.style.display = "none"
+        loginErrorText.innerText = "Неверный пароль"
+        loginError.style.display = "block";
+        setTimeout(()=>{
+            location.reload()
+        },1000)
     }
 })
